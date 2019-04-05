849f59ee303058fa4dfde9eae71e985057056e0f
yanqisen <842978971@qq.com>
2019-04-06 03:15:16 +0800

    update trans.py

--- data/trans.py
+++ data/trans.py
@@ -4,7 +4,7 @@ from utils import  URLDECODE
 
 a="/yk10/?page=54%20LIMIT%201%2C1%20UNION%20ALL%20SELECT%20NULL%2C%20NULL%2C%20NULL%23"
 print(URLDECODE(a))
-f=open("")
+f=open("test.txt",'a',encoding="utf-8")
 for line in open('test_normal.txt','r'):
     if len(line):
-        urldecode=urllib.parse.unquote(line)
+        f.write(line)
