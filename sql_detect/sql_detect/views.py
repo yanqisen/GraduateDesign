e8125a3e2f154e0eea824a5b9cd8c106e9e1a7b0
yanqisen <842978971@qq.com>
2019-04-07 23:58:26 +0800

    add attack test web

---
+++ sql_detect/sql_detect/views.py
@@ -0,0 +1,25 @@
+#import sys
+#sys.path.insert(1,'/home/yanqisen/Documents/GraduateDesign/CNN-SQL')
+from test import init, check,convert2label
+import os
+import json
+from django.http import HttpResponse
+from django.shortcuts import render
+import urllib.parse
+
+def index(request):
+    context={}
+    test=request.POST.get('test')
+    test2=request.POST.get('test2')
+    print(test)
+    print(test2)
+    if test:
+        urlcodestr=urllib.parse.quote(str(test))
+        model,w_model=init()
+        checked=check(model,w_model,urlcodestr)
+        result=convert2label(checked)
+        print(result)
+        context['res']=result
+    else:
+        context={}
+    return render(request, 'index.html', context)
---
+++ sql_detect/test.py
@@ -0,0 +1,60 @@
+#encoding:utf-8
+from keras.models import load_model
+from utils import URLDECODE
+from gensim.models.word2vec import Word2Vec
+from word import getVecs
+import keras
+import sys
+import numpy as np
+def init():	      
+    model=load_model('bestcnn')
+    w_model=Word2Vec.load("file/word2model")
+    return model,w_model
+def check(model,w_model,data):
+    if data !=None:
+        xx=[]
+        filepath='file/INPUT_SHAPE'
+        input_shape=[]
+        with open(filepath,'r') as f :
+            for line in f.readlines() :
+                input_shape=int(line)
+        if len(data.strip()): #判断是否是空行
+            for text in URLDECODE(data) :
+                #print(text)
+                try:
+
+                    xx.append(w_model[text])
+                except KeyError:
+                    continue
+            xx=np.array(xx, dtype='float')
+
+        #x=np.expand_dims(xx,1)
+        if not len(xx):
+            return [0]
+        x=[]
+        x.append(xx)
+        x=np.array(x)
+        #print(x)
+        x=keras.preprocessing.sequence.pad_sequences(x,dtype='float32',maxlen=input_shape)
+
+        result=model.predict_classes(x, batch_size=len(x))
+        #print(result)
+        return result
+    else:
+        return [0]
+
+                
+
+
+def convert2label(vector):
+    for v in vector:
+        if v==1:
+            string_array='sql inject'
+        elif v==2:
+            string_array='xss attack'
+        else:
+            string_array='normal data'
+
+    return string_array
+    
+
